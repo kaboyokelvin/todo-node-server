2135e3b1f28ea7e7d58e74a0e014ff88f969ce20
nyabongoedgar <nyabongoedgar@gmail.com>
2022-09-25 01:42:11 +0300

    initialize app as node js and fix bugs

--- server.js
+++ server.js
@@ -1,49 +1,51 @@
 const express = require("express");
 
 const app = express();
-// GET ==> 200 successful, PUT ==> 200, POST ==>  201, PATCH ==> 200, DELETE ==> 200
-// status , >= 400 -- things have broken or failed
-const todos = [];
+
+app.use(express.json());
+
+let todos = [];
+
+// GET ==> retrieves a resource, POST ==> adds a resource, PUT ==> modifies a resource, DELETE ==> deletes a resource 
 
 app.get("/todos", (req, res) => {
   res.status(200).json({ todos });
 });
 
 app.post("/todos", (req, res) => {
-  /**
-   * {title: string}
-   */
-  const body = req.body;
-  todos.push({ title: body.title, id: Math.random() });
+  const values = req.body;
+  todos.push({ title: values.title, id: Math.random() });
   res.status(201).json({
     message: "todo added successfully",
   });
 });
 
 app.put("/todos/:id", (req, res) => {
-  const params = req.params; // {id: 1, t:val}
+  const params = req.params;
   const id = params.id;
   const body = req.body;
   let newRecord;
-  todos.map((item) => {
-    let obj = { ...item };
-    if (item.id === id) {
+  const newResults = todos.map((item) => {
+    let obj = Object.assign({}, item);
+    if (item.id == id) {
       obj.title = body.title;
       newRecord = obj;
+      return obj;
     }
     return obj;
   });
-  res.status(201).json({
+  todos = newResults;
+  res.status(200).json({
     message: `todo with id ${id} has been updated`,
     newRecord,
   });
 });
 
 app.delete("/todos/:id", (req, res) => {
-    const params = req.params; // {id: 1, t:val}
+    const params = req.params;
     const id = params.id;
-    const newRecord = todos.filter((item) => item.id !== id);
-    todos = newRecord;
+    let todoIndex = todos.findIndex(item => item.id == id);
+    todos.splice(todoIndex, 1);
     res.status(201).json({
       message: `todo with id ${id} has been deleted`,
       todos,
